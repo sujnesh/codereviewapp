c2e369a0acc5990baedd1efee39dee9f7233262d
sujnesh-m <sujnesh.mishra@housing.com>
2023-03-19 18:11:42 +0530

    Code review app changes

---
+++ src/main/java/com/nivesh/codereviewapp/CodeReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.nivesh.codereviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nivesh/codereviewapp/CodeReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivesh.codereviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
